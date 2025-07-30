52f46255ac25c75c8c80075557b87c524a3a7893
Roshan Khan <roshankhan0885@gmail.com>
2025-07-30 23:31:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/weather", async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public'));
 });
 
 
